a7a809fa579456ee18639441dd6a5cc546a74ee0
Lorenzo Scaturchio <lorenzosca7@gmail.com>
2019-12-07 13:15:15 -0800

    Modified the dashboard

--- client/src/component/TeamCode/index.jsx
+++ client/src/component/TeamCode/index.jsx
@@ -8,10 +8,6 @@ class ReferralCode extends React.Component {
         <div id="Referral">
             <div className = "referral-code">
                 <h1> Referral Code </h1>
-                </div>
-                <div className= "referralcode">
-                    <h3>Share your code</h3>
-                
             </div>
         </div>
        );
