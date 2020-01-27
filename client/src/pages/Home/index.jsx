bd6ed30b8bd4979d9a928bcfbc151cc2b7f2270d
Shivanshu Gupta <shivanshu.gupta@gmail.com>
2020-01-26 20:18:46 -0800

    reordered FAQ to correct position

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -12,10 +12,10 @@ class Home extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <FAQ />
         <Title />
         <About />
         <Schedule />
+		<FAQ />
         <Sponsors />
       </React.Fragment>
     );
