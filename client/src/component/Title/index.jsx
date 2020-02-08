7b120f99377a20d5f8c9a974b8936409b65b3e76
Busher <bridibusher@gmail.com>
2020-02-08 13:56:17 -0800

    Fixed mobile frienldy code

--- client/src/component/Title/index.jsx
+++ client/src/component/Title/index.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -5,29 +6,6 @@ import "./title.scss";
 
 class Title extends React.Component {
   render() {
-    const isMobile = window.innerWidth <= 500;
-
-    if (isMobile) {
-      return (<div id="titleMobile">
-      {/* <section id="spacerMobile"></section> */}
-      <section id="contentMobile" >
-        <div>
-          <h1>HackMerced V</h1>
-          <h3>February 28th - March 1st</h3>
-          <button id="registerMobile">
-            <div id="buttonMobile">
-              <div id="containerMobile">
-                <Link to="/signUp" id="btnMobile">
-                  Register
-                </Link>
-              </div>
-            </div>
-          </button>
-        </div>
-      </section>
-    </div>);
-    }
-    else{
     return (
       <div id="title">
         <section id="spacer"></section>
@@ -49,7 +27,6 @@ class Title extends React.Component {
       </div>
     );
   }
-}
 }
 
 export default Title;
