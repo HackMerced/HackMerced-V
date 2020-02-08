792aed97e40b7057cb22dd31a933df72bf1e8a14
Busher <bridibusher@gmail.com>
2020-02-07 23:07:26 -0800

    Added code to detect if client is mobile, then run mobile friendly CSS

--- client/src/component/Title/index.jsx
+++ client/src/component/Title/index.jsx
@@ -5,6 +5,29 @@ import "./title.scss";
 
 class Title extends React.Component {
   render() {
+    const isMobile = window.innerWidth <= 500;
+
+    if (isMobile) {
+      return (<div id="titleMobile">
+      {/* <section id="spacerMobile"></section> */}
+      <section id="contentMobile" >
+        <div>
+          <h1>HackMerced V</h1>
+          <h3>February 28th - March 1st</h3>
+          <button id="registerMobile">
+            <div id="buttonMobile">
+              <div id="containerMobile">
+                <Link to="/signUp" id="btnMobile">
+                  Register
+                </Link>
+              </div>
+            </div>
+          </button>
+        </div>
+      </section>
+    </div>);
+    }
+    else{
     return (
       <div id="title">
         <section id="spacer"></section>
@@ -26,6 +49,7 @@ class Title extends React.Component {
       </div>
     );
   }
+}
 }
 
 export default Title;
