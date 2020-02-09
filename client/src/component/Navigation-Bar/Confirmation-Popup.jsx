286bb13918ac0239ab7d1b21c242e518fb49a610
Tuzette <tavu2013@gmail.com>
2020-02-08 21:38:44 -0800

    Navigation Bug with the Confirmation Box

---
+++ client/src/component/Navigation-Bar/Confirmation-Popup.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./navigation-bar.scss";
+
+//Popup Box
+//<!DOCTYPE html>
+<html>
+<body>
+
+<p>Click the button to display a confirm box.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  confirm("All unsaved work will be deleted")
+}
+<section>
+    var location = window.location.href;
+    if ((location == http://hackmerced.io/signUp) && <btn onclick="myFunction()">NavBar</btn>)
+    {   
+        function confirmpopup()
+        {
+            var c=("All unsaved work will be deleted")
+            if(c==true)
+            //go to the navigation button they want
+            {
+                if(document.getElementById("Banner").onclick = function() )
+                {
+                    window.location.href = 'https://mlh.io/seasons/na-2020/events';
+                }
+                else if(document.getElementById("About-Us").onclick = function())
+                {
+                    window.location.href = 'http://hackmerced.io/#about';
+                }
+                else if(document.getElementById("Schedule").onclick = function() )
+                {
+                    window.location.href = 'http://hackmerced.io/#schedule';
+                }
+                else if(document.getElementById("FAQ").onclick = function() )
+                {
+                    window.location.href = 'http://hackmerced.io/#faq';
+                }
+                else if(document.getElementById("Sponsors").onclick = function() )
+                {
+                    window.location.href = 'http://hackmerced.io/#sponsors';
+                }
+                
+
+            }
+            else
+            //Stay on the page
+
+        }
+    }
+
+    </section>
+
+</script>
+
+</body>
+</html>
