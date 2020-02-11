f6bb99b31d2b4eab633c3194478f3264e61c5495
Shivanshu Gupta <shivanshu.gupta@gmail.com>
2020-02-10 18:39:42 -0800

    finished upcoming Dates

--- client/src/component/Major-Dates/index.jsx
+++ client/src/component/Major-Dates/index.jsx
@@ -6,21 +6,21 @@ class MajorDates extends React.Component {
 	render(){
 	  return (
         <div id="MajorDates">
-            <div className = "important-dates">
-              <header>
-              <h1> Upcoming Dates: </h1>
+            <div className = "dates-Container">
+              <header className="date-Title">
+                <h1> Upcoming Dates: </h1>
               </header>
-              <article>
-              <h1> Applications Open </h1>
-              <p>November 7th, 2019</p>
+              <article className="important-Date-gray">
+                <h1> Applications Open </h1>
+                <p>November 7th, 2019</p>
               </article>
-              <article>
-              <h1> Applications Close </h1>
-              <p>November 29th, 2019</p>
+              <article className="important-Date">
+                <h1> Applications Close </h1>
+                <p>November 29th, 2019</p>
               </article>
-              <article>
-              <h1> HackMerced 2020 </h1>
-              <p>Janurary 31st, 2020</p>
+              <article className="important-Date-gray">
+                <h1> HackMerced 2020 </h1>
+                <p>Janurary 31st, 2020</p>
               </article>
 
 			      </div>
