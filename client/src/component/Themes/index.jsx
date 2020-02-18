54a3d54e4a94991c26a7ffb430b29ac8dfbcb5cc
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-02-17 22:38:18 -0800

    resize images

--- client/src/component/Themes/index.jsx
+++ client/src/component/Themes/index.jsx
@@ -2,116 +2,27 @@ import React from "react";
 import "./themes.scss";
 
 //The theme pictures
-import SOCIAL from "../../assets/Images/social good.png";
-import SUSTAIN from "../../assets/Images/sustainability.png";
+import SOCIALGOOD from "../../assets/Images/social good.png";
+import SUSTAINABILITY from "../../assets/Images/sustainability.png";
 import HEALTH from "../../assets/Images/medical.png";
 
-class Themes extends React.Component{
-    constructor(props){
-        super(props);
-        this.themes = {
-            titles: [
-                "Social Good",
-                "Sustainability",
-                "Health",
-            ],
-            image: [
-                SOCIAL,
-                SUSTAIN,
-                HEALTH,
-            ],
-
-            alt: [
-                "social good",
-                "sustainability",
-                "health",
-            ],
-        }
-    };
-
-    displayThemes = ({titles, image, alt}) => {
-    let sections = [],
-        social_good = [],
-        sustainability = [],
-        health = [];
-
-        for(let i = 0; i < titles.length; ++i){
-            switch(sections[i]) {
-                default:
-                    break;
-                case "Social Good": {
-                    social_good.push(
-                        <div className="themes" titles={titles[i]}>
-                        <h3>Social Good</h3>
-                        <img src={image[i]} alt={alt[i]} />
-                        </div>
-                    );
-                    break;
-                }
-                case "Sustainability": {
-                    sustainability.push(
-                        <div className="themes" titles={titles[i]}>
-                        <h3>Sustainability</h3>
-                        <img src={image[i]} alt={alt[i]} />
-                        </div>
-                    );
-                    break;
-                }
-                case "Health": {
-                    health.push(
-                        <div className="themes" titles={titles[i]}>
-                        <h3>Health</h3>
-                        <img src={image[i]} alt={alt[i]} />
-                        </div>
-                    );
-                    break;
-                }
-            }
-
-            social_good.forEach(item => {
-                sections.push(<div id="social good">{item}</div>);
-              });
-          
-              sustainability.forEach(item => {
-                sections.push(<div id="sustainability">{item}</div>);
-              });
-          
-              health.forEach(item => {
-                sections.push(<div id="health">{item}</div>);
-              });
-
-            return sections;
-        }
-    }
-    
-    render(){
-        return(
-            <section id="themes">
-                <h2>Themes</h2>
-                {/* <div class="grid-container">{this.displayThemes(this.themes)} */}
-                <div class="grid-container">
-                    <div class="grid-item">
-                        <h3>Social Good</h3>
-                    </div>
-                    <div class="grid-item">
-                        <h3>Sustainability</h3>
-                    </div>
-                    <div class="grid-item">
-                        <h3>Health</h3>
-                    </div>
-                    <div class="grid-item">
-                        <img src={SOCIAL} />
-                    </div>
-                    <div class="grid-item">
-                        <img src={SUSTAIN}/>
-                    </div>
-                    <div class="grid-item">
-                        <img src={HEALTH}/>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+class Themes extends React.Component {
+  render() {
+    return (
+      <section id="themes">
+        <h2>Themes</h2>
+        <div class="grid-container">
+        <img id="social-good" className="grid-item-image" src={SOCIALGOOD} alt="social good" />
+          <img id="sustainability" className="grid-item-image" src={SUSTAINABILITY} alt="sustainability" />
+          <img id="health" className="grid-item-image" src={HEALTH} alt="health" />
+          <h3 className="grid-item-title">Social Good</h3>
+          <h3 className="grid-item-title">Sustainability</h3>
+          <h3 className="grid-item-title">Health</h3>
+         
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Themes;
